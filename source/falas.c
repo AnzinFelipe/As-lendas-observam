78b63f745ca4c1dec334c273f1c335ac74b58361
AnzinFelipe <ian.felipe.drunen@gmail.com>
2026-05-25 20:25:55 -0300

    Ajeitando texto do boca de ouro

--- source/falas.c
+++ source/falas.c
@@ -25,7 +25,7 @@ RayDialComponent *criarComp(const char *titulo, const char *texto) {
 
 RayDialComponent *criarComp_ouro(const char *titulo, const char *texto) {
     RayDialComponent *comp = CreatePortraitDialogue(
-        (Rectangle){25, 420, 850, 400},
+        (Rectangle){25, 420, 800, 450},
         titulo,
         texto,
         (Color){0, 0, 0, 0}
